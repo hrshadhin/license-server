40ae1bd5953b6f3a53965e5c6d2dddc6b9128cf2
H.R. Shadhin <hrshadhin@pathao.com>
2019-11-05 18:24:20 +0600

    ctl+c, kill, sigterm support added

--- main.go
+++ main.go
@@ -7,25 +7,32 @@ import (
 	"net/http"
 	"os"
 	"os/signal"
+	"syscall"
 	"time"
 
 	"github.com/gorilla/mux"
 	"github.com/hrshadhin/license-server/controllers"
 	"github.com/hrshadhin/license-server/middleware"
+	u "github.com/hrshadhin/license-server/utils"
 
-	//"github.com/getsentry/sentry-go"
-	//newrelic "github.com/newrelic/go-agent"
-	//nrgorilla "github.com/newrelic/go-agent/_integrations/nrgorilla/v1"
+	"github.com/getsentry/sentry-go"
+	newrelic "github.com/newrelic/go-agent"
+	nrgorilla "github.com/newrelic/go-agent/_integrations/nrgorilla/v1"
 )
 
 var (
+	listenHost string
 	listenPort string
+	listenAddr string
 )
 
 func main() {
 
-	flag.StringVar(&listenPort, "port", ":8000", "server listen port")
+	flag.StringVar(&listenHost, "host", "127.0.0.1", "server listen host")
+	flag.StringVar(&listenPort, "port", "8000", "server listen port")
 	flag.Parse()
+
+	listenAddr = listenHost + ":" + listenPort
 
 	logger := log.New(os.Stdout, "LServer: ", log.LstdFlags)
 
@@ -33,7 +40,13 @@ func main() {
 	done := make(chan bool, 1)
 	quit := make(chan os.Signal, 1)
 
-	signal.Notify(quit, os.Interrupt)
+	// Catch the Ctrl-C and SIGTERM from kill command
+	signal.Notify(quit, os.Interrupt, os.Kill, syscall.SIGTERM)
+
+	//sentry logging
+	sentry.Init(sentry.ClientOptions{
+		Dsn: u.MustGetEnv("sentry_dns"),
+	})
 
 	//define new server
 	server := newWebserver(logger)
@@ -42,25 +55,13 @@ func main() {
 	go shutdownWebserver(server, logger, quit, done)
 
 	//now start the server
-	logger.Println("Server is ready to handle requests at", listenPort)
+	logger.Println("Server is ready to handle requests at", listenAddr)
 	if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-		logger.Fatalf("Could not listen on %s: %v\n", listenPort, err)
+		logger.Fatalf("Could not listen on %s: %v\n", listenAddr, err)
 	}
 
 	<-done
 	logger.Println("Server stopped")
-
-	//sentry.Init(sentry.ClientOptions{
-	//	Dsn: u.MustGetEnv("sentry_dns"),
-	//})
-	//
-	//cfg := newrelic.NewConfig("License Server", u.MustGetEnv("new_relic_license_key"))
-	//app, err := newrelic.NewApplication(cfg)
-	//if nil != err {
-	//	fmt.Println(err)
-	//	os.Exit(1)
-	//}
-	//err = http.ListenAndServe(":"+port, nrgorilla.InstrumentRoutes(router, app))
 
 }
 
@@ -94,10 +95,21 @@ func newWebserver(logger *log.Logger) *http.Server {
 	router.NotFoundHandler = http.HandlerFunc(middleware.NotFoundHandler)
 	router.Use(middleware.JwtAuthentication) //attach JWT auth middleware
 
+	//newrelic handler
+	cfg := newrelic.NewConfig("License Server", u.MustGetEnv("new_relic_license_key"))
+	app, err := newrelic.NewApplication(cfg)
+	if nil != err {
+		logger.Fatalf("Newrelic couldn't integrate: %v\n", err)
+		os.Exit(1)
+	}
+
+	final_router := nrgorilla.InstrumentRoutes(router, app)
+
+
 	//server instance
 	return &http.Server{
-		Addr:         listenPort,
-		Handler:      router,
+		Addr:         listenAddr,
+		Handler:      final_router,
 		ErrorLog:     logger,
 		ReadTimeout:  5 * time.Second,
 		WriteTimeout: 10 * time.Second,
